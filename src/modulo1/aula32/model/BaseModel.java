3ee53c4b3c9976fb8560a18b1536da55d4a9780c
meimoon1 <melevelyn69@gmail.com>
2021-11-04 17:30:53 -0300

    models

--- src/modulo1/aula32/model/BaseModel.java
+++ src/modulo1/aula32/model/BaseModel.java
@@ -1,1 +1,21 @@
 package model;
+public abstract class BaseModel{
+    public int id;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof BaseModel){
+            BaseModel outro = (BaseModel)obj;
+            if(outro.id == this.id){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        //int não tem toString então passa o objeto pra ser convertido para string
+        return Integer.toString(this.id);
+    }
+}
