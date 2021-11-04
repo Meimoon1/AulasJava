278a71421131cd9e79cdf37dae4abb10fef0df7a
meimoon1 <melevelyn69@gmail.com>
2021-11-04 17:21:59 -0300

    atp

---
+++ src/modulo1/aula28/P1/view/Escrita.java
@@ -0,0 +1,28 @@
+package P1.view;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Escrita {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fos = new FileOutputStream("P1/dados/molhos.txt");
+            //criar fluxo de leitura
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            bw.write("Sugo\n");
+            bw.write("Branco");
+
+            bw.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Não foi possível encontrar o arquivo");
+        }catch (IOException e){
+            System.out.println("Não foi possível encontrar o arquivo");
+        }
+    }
+}
