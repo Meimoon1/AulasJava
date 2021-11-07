32ef6e567cfc69c8b2140cfa47147d5788d1d17d
meimoon1 <melevelyn69@gmail.com>
2021-11-07 18:02:42 -0300

    aula

--- src/modulo1/aula32/P2/View.java
+++ src/modulo1/aula32/P2/View.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class View {
     public static void main(String[] args) {
@@ -7,14 +10,35 @@ public class View {
         c1.marca = "Vw";
         c1.modelo = "Fusca";
 
-        
+        //transformando objeto p texto
         try {
             FileWriter fw = new FileWriter("Carros.csv",true);
-            String carroString = String.format("%s;%s",c1.marca, c1.modelo);
+            //DAO - data access object, ele pega um dado e converte p objeto e vice versa
+            String carroString = String.format("%s;%s\n",c1.marca, c1.modelo);
             fw.write(carroString);
             fw.close();
-        } catch (IOException e) {
+        } 
+        catch (IOException e) {
             System.out.println("Não foi possível abrir o arquivo");
         }
+
+        //transformando texto p obj
+        try {
+            Scanner sc = new Scanner(new File("Carros.csv"));
+            while(sc.hasNextLine()){
+                //fazendo um dao
+                String linha = sc.nextLine();
+                String[] carroString = linha.split(";");
+
+                Carro c = new Carro();
+                c.marca = carroString[0];
+                c.modelo = carroString[1]; 
+                System.out.printf("%s-%s \n",c.marca,c.modelo);
+                
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Não foi possível abrir o arquivo");        }
+
+
     }
 }
